97bcf7feae8ead0f4d3512f1b9eabb956c887dda
Aline Fernanda Bellozo <alinebellozo@gmail.com>
2021-05-22 20:12:37 -0300

    Projeto bytebank-herdado-conta

---
+++ src/TesteContas.java
@@ -0,0 +1,17 @@
+
+public class TesteContas {
+
+	public static void main(String[] args) {
+		
+		ContaCorrente cc = new ContaCorrente(1234, 5678);
+		cc.deposita(1200.0);
+		
+		ContaPoupanca cp = new ContaPoupanca(4321, 8765);
+		cp.deposita(1000.0);
+		
+		cc.transfere(10.0, cp);
+		
+		System.out.println("CC: " + cc.getSaldo());
+		System.out.println("CP: " + cp.getSaldo());
+	}
+}

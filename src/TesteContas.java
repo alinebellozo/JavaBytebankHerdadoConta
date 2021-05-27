53504d6285c3aac4e16a50e6ca7c23676b2a41ac
Aline Fernanda Bellozo <alinebellozo@gmail.com>
2021-05-27 17:07:54 -0300

    Alterações com exceções

--- src/TesteContas.java
+++ src/TesteContas.java
@@ -1,7 +1,7 @@
 
 public class TesteContas {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SaldoInsuficienteException {
 		
 		ContaCorrente cc = new ContaCorrente(1234, 5678);
 		cc.deposita(1200.0);
